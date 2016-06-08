da1e2fe99b6589f4c853ec65586a7812eb667f9e
world <eatmyhotspam@gmail.com>
2016-06-07 20:30:21 -0700

    tests pass.

---
+++ README.md
@@ -0,0 +1,43 @@
+# TL;DR 
+
+# Where lodash is a huge box of bad-ass tools with great docs, ithas is a sharpened, surgical, sugary steel blade... only with terrible docs, endless 's' words , and way big fonts. But... like any powerful [harlequin](http://www.ehow.com/info_12332443_harlequin-ice-cream.html) ninja, ithas, comes to you in a 3-pack of delciousnes magicalness:
+
+## ithas.data 
+
+**format**: `ithas.data(anyDataType, path)`
+
+**returns:** `boolean`
+
+tests ALL data types and as many keys deep as you wish (if checking object) to see if there is any data anywhere, in any of it. undefined, null, NaN, "", [], [""], {}, {key:null}, etc, etc, ad naeseum all return false. Good luck doing that on lodash lol. 49 cases covered by tests and more all the time. Fire and forget! Has a deep setting coming soon that is currently disabled for performance reasons.
+
+```
+ithas.data(0) == true
+ithas.data("") == false
+ithas.data([]) == false
+ithas.data({} == false
+ithas.data({key:null}) == false
+ithas.data({key:0}) == true
+ithas.data({key:null}, 'key') == false
+ithas.data({key:undefined}, 'key') == false
+ithas.data({key:NaN}, 'key') == false
+... 49 tested cases and counting...
+```
+
+----------------------------------------------------------
+## ithas.key
+
+**format**: `ithas.key(obj, path)`
+
+**returns:** `boolean`
+
+WTF brah? Have you ever heard of lodash _.has? Yep. And it's mainly reliable. But, it evaluates false in scary places where things are wrapped that you don't know about (because they may call a toObject prototype behind the scenes to make it look and smell like an object, or JSON, but it's not!) Here's an example: if you try _.has against a mongoose document you just fetched, you will be suprised to see _.has declares that you have no keys! Boom! You just got lodashed in your balls!
+
+Wait, wait, what about using _.get to check for undefined keys or values? Sure that works. What if the value is null? Nan? empty string? Empty array? Another nested key of emptiness? Sorry. 
+
+----------------------------------------------------------
+COMING SOON!!!!!!!!!!!!!!!!!!!!! ## ithas.value
+**format**: `ithas.key(obj, path)`
+## ithas.value(obj, path, value)
+Pretty basic but pretty magic. Just pass it an object, a path as deep as you want in string format, and then value you want to evaluate for. Handle's all the weird cases of null, undefined, keys not there, wrapped crap, etc.  
+
+# ps I apologize for my shitty code. I am currently busy levelling up.
---
+++ index.js
@@ -0,0 +1,105 @@
+// super magical 8ball function. pass it 1-3 params and never shall it probably most not lie
+
+var ithas = function() {}
+var _     = require('lodash')
+
+// TODO: make these not globally scoped
+var HAS_DATA_CNT = 0
+var HAS_KEY_CNT  = 0
+
+//ithas.data has been cleaned up
+ithas.data = function(obj, str) {
+
+  // faster shallower check
+  const IsData = item => !(_.isUndefined(item) || _.isNull(item) || _.isNaN(item) || item === "")
+
+  // SAVE AS A FUTURE OPTION FOR A FOR A DEEPER BUT SLOWER CHECK - that's what said
+  // const IsData = item => !(_.isUndefined(item) || _.isNull(item) || _.isNaN(item) || item === "" || IsObjThatHasNoData(item)) 
+
+  const IsObjThatHasNoData = item => _.isArray(item) || _.isObject(item) ? !_.some(_.values(item), IsData) : false
+
+  const IsObjThatHasData = item => _.isObject(item) || _.isArray(item) ? _.some(item, IsData) : false
+
+  // a warning to help us keep our event loop lowfat
+  if (++HAS_DATA_CNT % 10000 === 0 && DEV)  b('WOAH LOTS OF HAS DATA CALLS', HAS_DATA_CNT)
+
+  // 1 param check
+  if (arguments.length === 1) {
+    var arg = arguments[0]
+    if (IsObjThatHasData(arg)) return true
+    if (IsObjThatHasNoData(arg)) return false
+    if (_.isNumber(arg)) return !_.isNaN(arg)
+    if (_.isBoolean(arg)) return true
+    return _.size(arg) ? true : false // handles null, undefined, and empty string
+  // 2 param check
+  } else if (arguments.length === 2) {
+    if (!_.isObject(arguments[0]) && !_.isString(arguments[1])) return false
+
+    // i thought these mattered but tests pass when commented out... and our boxes are still standing... *shrug*
+    // if (HasValue(obj, str, undefined)) return false
+    // if (HasValue(obj, str, null)) return false
+    // if (HasValue(obj, str, '')) return false
+
+    if (ithas.key(obj, str)) {
+      // building the str as dot notation into an Object
+      var keys = str.split('.')
+      // stitch the str of children keys to the parent obj:
+      var key = keys.shift()
+      // start at 1st node and walk...
+      var node = obj[ key ]
+      // use this while loop to get to the final node (in dot notation)
+      while (key = keys.shift()) node = node[ key ]
+      return ithas.data(node)
+    }
+    return false
+  }
+}// has.data
+
+//ithas.key has not yet been cleaned up but the shit works... 
+ithas.key = function(obj, str) {
+  if (++HAS_KEY_CNT % 10000 === 0 && DEV) b('WOAH LOTS OF HAS KEY CALLS', HAS_KEY_CNT)
+  var keys, key;
+
+  // return false if obj is either null or undefined
+  if (obj === null) return false
+
+  if (!str || !str.length || Object.prototype.toString.call(str) !== "[object String]") {
+      return false;
+  }
+
+  //if parent object does not exist and we aren't trying to create
+  if (!obj || obj !== Object(obj)) {
+      return false;
+  }
+
+  // if obj is not an oject return false
+  if (!_.isObject(obj)) return false 
+
+  // walk the object
+  keys = str.split(".");
+  var walk = "object";
+  while (keys.length)
+  {
+    key = keys.shift();
+    walk += "." + key;
+
+    //if it's not an object, make it one
+    //if obj doesn't contain the key, add it and set it to an empty object
+    if (!obj || !key || obj[key] === undefined) {
+      return false
+    } else if (typeof obj[key] === "undefined") {
+      return false
+    } else if (!(key in obj)) {
+      console.error("[HasKey] This " + walk + " is null")
+     return false
+    } else if (key in obj && !keys.length) {
+      //console.log("Sucessfully walked",walk);
+      return true
+    }
+
+    obj = obj[key]
+  }
+  return false;
+}//ithas.key
+
+module.exports = ithas
